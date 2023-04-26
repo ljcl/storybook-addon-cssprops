5762b0ba3bfaf7622e23c6afe983e8331eb708cc
Luke Secomb <13529535+lukethacoder@users.noreply.github.com>
2023-04-26 16:35:52 +1000

    fix title count to exclude config keys (`presetColors`, `disable`)

--- packages/storybook-addon-cssprops/src/title.ts
+++ packages/storybook-addon-cssprops/src/title.ts
@@ -2,9 +2,11 @@ import { useParameter } from "@storybook/api";
 import { PARAM_KEY } from "./constants";
 import { CssPropertyItemGroup } from "./components/CssPropsTable/types";
 
+const CONFIG_KEYS = ['presetColors', 'disable']
+
 export function useTitle(): string {
   const cssprops = useParameter<CssPropertyItemGroup>(PARAM_KEY, {});
-  const controlsCount = Object.values(cssprops).length;
+  const controlsCount = Object.entries(cssprops).filter((item) => !CONFIG_KEYS.includes(item[0])).length;
   const suffix = controlsCount === 0 ? "" : ` (${controlsCount})`;
   return `CSS Custom Properties${suffix}`;
 }
